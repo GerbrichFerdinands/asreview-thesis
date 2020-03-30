62a32be1fd3a6d62923668a4dc50693d158b8223
Gerbrich Ferdinands <gerbrichferdinands@gmail.com>
2020-03-30 10:16:05 +0200

    update hp optimization checklists

--- simulation_study/R/00_condition_grid.R
+++ simulation_study/R/00_condition_grid.R
@@ -19,17 +19,17 @@ f <- c("T", # tf-idf
 ) 
 
 # balance strategy
-b <- c(#"N",# no reblancing 
-       "D", # dynamic undersampling
+b <- c(#"S",# simple, no reblancing 
+       "D" # double (dynamic supersampling)
        #"U" # undersampling (as a bonus!)
 )
 
 conditiongrid <- tibble(condition = do.call(paste0, expand.grid(c, q, f, b)))
 
-conditiongrid$stage <- factor(c(rep(1:2, each = 5), rep(3,10)), labels = c("classifier", "doc2vec")) #, "AU"))
+#conditiongrid$stage <- factor(c(rep(1:2, each = 5), rep(3,10)), labels = c("classifier", "doc2vec")) #, "AU"))
 # stage 1 = all classifiers + tfidf, stage 2 is repeat with doc2vec, stage 3 is repeat with agressive undersampling. 
 
-saveRDS(conditiongrid, file = "simulation/00_conditiongrid.RDS")
-saveRDS(c("ace", "nudging", "ptsd", "software", "wilson"), file = "simulation/00_datasets.RDS")
+saveRDS(conditiongrid, file = "R/00_conditiongrid.RDS")
+saveRDS(c("ace", "nudging", "ptsd", "software", "virus", "wilson"), file = "R/00_datasets.RDS")
 
         
--- simulation_study/hpoptimization/input.sh
+++ simulation_study/hpoptimization/input.sh
@@ -1,2 +1,4 @@
 !#/bin/bash
-./cart_hyper_run.py hyper-active -t 5 -m svm -b double -e tfidf -q max -d ace -r 23 # job id 7768402
+./cart_hyper_run.py hyper-active -t 5 -m svm -b double -q max -e tfidf -q max -d ace -r 23 # job id 7768402
+
+./cart
